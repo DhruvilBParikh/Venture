052f4240b7ed19b40bd696569147f24623ace106
Shivang Mistry <shivangmistry96@gmail.com>
2020-04-10 22:08:56 -0700

    Merged login and signup fragments

---
+++ app/src/main/java/com/example/venture_v0/Fragments/signup/SignupFragment.java
@@ -0,0 +1,131 @@
+package com.example.venture_v0.Fragments.signup;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.venture_v0.MainActivity;
+import com.example.venture_v0.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignupFragment extends Fragment {
+
+    private static final String TAG = "SignupFragment";
+
+    private View view;
+    private Button signupButton;
+    private EditText nameText, emailText, passwordText;
+
+    public SignupFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Log.d(TAG, "onCreateView: next fragment to open: " + getTag());
+        view = inflater.inflate(R.layout.fragment_signup, container, false);
+
+        signupButton = view.findViewById(R.id.signup_button);
+        nameText = view.findViewById(R.id.nameText);
+        emailText = view.findViewById(R.id.emailText);
+        passwordText = view.findViewById(R.id.passwordText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean name = nameCheck();
+                boolean email = emailCheck();
+                boolean password = passwordCheck();
+
+                if(name && email && password) {
+                    // handle sign up code
+
+                    ((MainActivity) getActivity()).logsIn(getTag());
+
+                } else {
+                    if(!name) {
+                        Log.d(TAG, "onClick: invalid name message");
+                        view.findViewById(R.id.invalidName).setVisibility(View.VISIBLE);
+                    } else {
+                        view.findViewById(R.id.invalidName).setVisibility(View.GONE);
+                    }
+                    if(!email) {
+                        Log.d(TAG, "onClick: invalid email message");
+                        view.findViewById(R.id.invalidEmail).setVisibility(View.VISIBLE);
+                    } else {
+                        view.findViewById(R.id.invalidEmail).setVisibility(View.GONE);
+                    }
+                    if(!password) {
+                        Log.d(TAG, "onClick: invalid password message");
+                        view.findViewById(R.id.invalidPassword).setVisibility(View.VISIBLE);
+                    } else {
+                        view.findViewById(R.id.invalidPassword).setVisibility(View.GONE);
+                    }
+                }
+            }
+        });
+
+        return view;
+    }
+
+    public boolean nameCheck() {
+        String name = nameText.getText().toString().trim();
+        if(name.equals("")) {
+            Log.d(TAG, "onFocusChange: name in empty");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean passwordCheck() {
+        String password = passwordText.getText().toString();
+        if(password.equals("")) {
+            Log.d(TAG, "passwordCheck: password is empty");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean emailCheck()
+    {
+        String regExpn =
+                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+        CharSequence inputStr = emailText.getText().toString();
+
+        Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+
+        if(matcher.matches()) {
+            Log.d(TAG, "emailCheck: email is valid");
+            return true;
+        } else {
+            Log.d(TAG, "emailCheck: email is invalid");
+            return false;
+        }
+    }
+
+}
--- app/src/main/java/com/example/venture_v0/MainActivity.java
+++ app/src/main/java/com/example/venture_v0/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.venture_v0;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
@@ -14,6 +13,8 @@ import com.example.venture_v0.Fragments.explore.ExploreFragment;
 import com.example.venture_v0.Fragments.history.HistoryFragment;
 import com.example.venture_v0.Fragments.loginSignup.LoginSignupFragment;
 import com.example.venture_v0.Fragments.plan.PlanFragment;
+import com.example.venture_v0.Fragments.login.LoginFragment;
+import com.example.venture_v0.Fragments.signup.SignupFragment;
 import com.example.venture_v0.Fragments.profile.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,22 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigation;
     private Boolean isLoggedIn;
-    private Fragment exploreFragment;
-    private Fragment planFragment ;
-    private Fragment addEventFragment;
-    private Fragment historyFragment;
-    private Fragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        exploreFragment = ExploreFragment.newInstance("", "");
-        planFragment = PlanFragment.newInstance("", "");
-        addEventFragment = AddEventFragment.newInstance("", "");
-        historyFragment = HistoryFragment.newInstance("", "");
-        profileFragment = ProfileFragment.newInstance("", "");
-
         setContentView(R.layout.activity_main);
 
         isLoggedIn = checkSession();
@@ -54,26 +43,40 @@ public class MainActivity extends AppCompatActivity {
 
         if(loginPrompt) {
             Log.d(TAG, "openFragment: user not logged in");
-            transaction.replace(R.id.container, LoginSignupFragment.newInstance("", ""), tag);
+            transaction.replace(R.id.container, new LoginSignupFragment(), tag);
         } else {
             switch(tag){
                 case "EXPLORE":
-                    transaction.replace(R.id.container, exploreFragment, tag);
+                    transaction.replace(R.id.container, new ExploreFragment(), tag);
                     break;
                 case "PLAN":
-                    transaction.replace(R.id.container, planFragment, tag);
+                    transaction.replace(R.id.container, new PlanFragment(), tag);
                     break;
                 case "ADDEVENT":
-                    transaction.replace(R.id.container, addEventFragment, tag);
+                    transaction.replace(R.id.container, new AddEventFragment(), tag);
                     break;
                 case "HISTORY":
-                    transaction.replace(R.id.container, historyFragment, tag);
+                    transaction.replace(R.id.container, new HistoryFragment(), tag);
                     break;
                 case "PROFILE":
-                    transaction.replace(R.id.container, profileFragment, tag);
+                    transaction.replace(R.id.container, new ProfileFragment(), tag);
                     break;
             }
         }
+        transaction.commit();
+    }
+
+    public void openLoginFragment(String tag) {
+        Log.d(TAG, "openLoginFragment: opening login fragment");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new LoginFragment(), tag);
+        transaction.commit();
+    }
+
+    public void openSignupFragment(String tag) {
+        Log.d(TAG, "openLoginFragment: opening login fragment");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new SignupFragment(), tag);
         transaction.commit();
     }
 
