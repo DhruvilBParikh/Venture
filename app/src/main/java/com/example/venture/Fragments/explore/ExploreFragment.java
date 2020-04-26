724a2d4461bfdfe00f2d4383ae533ae7fd8f8ea3
DhruvilBParikh <47414810+DhruvilBParikh@users.noreply.github.com>
2020-04-26 14:47:35 -0700

    Update ExploreFragment.java

--- app/src/main/java/com/example/venture/Fragments/explore/ExploreFragment.java
+++ app/src/main/java/com/example/venture/Fragments/explore/ExploreFragment.java
@@ -14,13 +14,6 @@ import com.example.venture.adapters.ExploreSectionsPageAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link ExploreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ExploreFragment extends Fragment {
 
     private static final String TAG = "ExploreFragment";
